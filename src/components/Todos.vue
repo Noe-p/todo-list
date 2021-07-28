e7b470f3ab6a266e3d33e334816104c37b1726e3
Noe-p <noephilippe29@gmail.com>
2021-07-28 10:19:53 +0200

    Ajout supprimer et modifier une note

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -28,6 +28,27 @@
       <h2>{{ todo.titre }}</h2>
       <p>{{ todo.description }}</p>
     </div>
+    <div class="edit-item">
+      <img @click="editItemOpen()" src="../assets/icons/edit.png" alt="edit"/>
+
+      <img @click="rmTodo(todo)" src="../assets/icons/trash.png" alt="trash"/>
+    </div>
+    <div class="edit">
+      <div @click="editItemClose()" class="editClose">✖</div>
+      <h2>Éditer une note :</h2>
+      <div class="titre">
+        <label for="titre">Titre</label>
+        <input type="text" name="titreEdit" :placeholder="[[ todo.titre ]]">
+      </div>
+      <div class="description">
+        <label for="description">Description</label>
+        <textarea type="text" cols="30" rows="5" name="descriptionEdit" :placeholder="[[ todo.description ]]"></textarea>
+      </div>
+      <p class="alertEdit"></p>
+      <div class="ajouter">
+         <input  @click="editTodo(todo)" type="submit" value="Ajouter"/>
+      </div>
+    </div>
   </div>
 
 
@@ -75,7 +96,40 @@ export default {
     },
     todoChecked(todo){
       todo.checked = !todo.checked;
+    },
+    rmTodo(todo){
+      this.todos.splice(this.todos.indexOf(todo),1);
+    },
+    editItemOpen(){
+      let edit = document.querySelector('.edit');
+      edit.classList.add('active');
+    },
+    editItemClose(){
+      let edit = document.querySelector('.edit');
+      edit.classList.remove('active');
+    },
+    editTodo(todo){
+      let titre = document.querySelector('input[name="titreEdit"]').value;
+      let description = document.querySelector('textarea[name="descriptionEdit"]').value;
+      let alert = document.querySelector('.alertEdit');
+
+      if(titre != ""){
+        if(description != ""){
+          this.todos.splice(this.todos.indexOf(todo),1,{
+            checked: 0,
+            titre: titre,
+            description: description
+          });
+        } else {
+          alert.setAttribute("style", "color: rgb(190, 16, 16)");
+          alert.textContent = "Veuiller entrer une description..."
+        }
+      } else {
+        alert.setAttribute("style", "color: rgb(190, 16, 16)");
+        alert.textContent = "Veuiller entrer un titre..."
+      }
     }
+
   }
 }
 
